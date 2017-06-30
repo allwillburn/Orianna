4caa5e761e32b92408535e7aa699d4013ae69fee
allwillburn <livefortoday470@gmail.com>
2017-06-29 20:36:21 -0400

    Update Orianna.lua

--- Orianna.lua
+++ Orianna.lua
@@ -1,4 +1,4 @@
-local ver = "0.02"
+local ver = "0.03"
 
 
 if FileExist(COMMON_PATH.."MixLib.lua") then
@@ -143,7 +143,7 @@ OnTick(function (myHero)
           	  
 
 			if OriannaMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, 825) then
-			 CastSkillShot(_E, myHero)
+			 CastSkillShot(_E, myHero.pos)
 	    end
              
            	  
